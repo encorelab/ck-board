35397d8947e9d89ec3f58d9585b05dac5c1c00cf
Ammar <ammart500@gmail.com>
2021-09-12 21:29:36 -0400

    Made task editable + redesigned settings modal

--- src/app/models/board.ts
+++ src/app/models/board.ts
@@ -1,6 +1,10 @@
 export class Board {
     boardID: string
     name: string;
+    task: {
+        title: string;
+        message?: string;
+    }
     bgImage?: {
         url: string;
         imgSettings: {};

de0a72b80545ee185c2bf363b058a64a17155b0d
Ammar <ammart500@gmail.com>
2021-11-07 12:49:09 -0500

    Fixed routing issues

--- src/app/utils/auth.guard.ts
+++ src/app/utils/auth.guard.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { Observable } from 'rxjs';
@@ -11,16 +11,21 @@ export class AuthGuard implements CanActivate {
   
   constructor(
     public authService: AuthService,
-    public router: Router
+    public router: Router,
+    public ngZone: NgZone
   ){ }
 
   canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    if (this.authService.isLoggedIn !== true) {
-      this.router.navigate(['/login'])
-    }
-    return true;
+    _next: ActivatedRouteSnapshot,
+    _state: RouterStateSnapshot): Promise<boolean> {
+    
+    const isLoggedIn = new Promise<boolean>((resolve, reject) => {
+      this.authService.getAuthenticatedUser().then((user) => {
+        resolve(user != null)
+      })
+    })
+
+    return isLoggedIn
   }
 
 }

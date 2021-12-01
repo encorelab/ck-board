04c5ea47f1b100aa27d05f209fe3b84bfd46a740
StevenWuzz <steven.limantoro@gmail.com>
2021-12-01 17:52:15 +0700

    feat: frontend for resetting password

--- src/app/components/password-reset/password-reset.component.ts
+++ src/app/components/password-reset/password-reset.component.ts
@@ -1,18 +1,58 @@
-import { E } from '@angular/cdk/keycodes';
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({ 
     templateUrl: 'password-reset.component.html', 
     styleUrls: ['./password-reset.component.scss']
 })
-export class PasswordResetComponent{
+export class PasswordResetComponent implements OnInit, OnDestroy{
 
     email: string
     msg: string
 
-    constructor(private auth: AuthService) {}
+    ngUnsubscribe: Subject<any> = new Subject<any>();
+
+    mode: string;
+    oobCode: string;
+
+    newPassword: string;
+    confirmPassword: string;
+
+    oobCodeChecked: boolean;
+
+    constructor(private auth: AuthService, private activatedRoute: ActivatedRoute, private router: Router) {}
+
+    ngOnInit() {
+        this.activatedRoute.queryParams
+          .pipe(takeUntil(this.ngUnsubscribe))
+          .subscribe(params => {
+            if (!params) {
+                this.router.navigate(['/login']);
+            }
+
+            this.mode = params['mode'];
+            this.oobCode = params['oobCode'];
+
+            if(this.mode == "resetPassword") {
+                this.auth.getAuth().verifyPasswordResetCode(this.oobCode)
+                .then(() => {
+                    this.oobCodeChecked = true;
+                })
+                .catch(e => {
+                    alert(e);
+                    this.router.navigate(['/password/reset']);
+                });
+            }
+        })
+    }
+    
+    ngOnDestroy(): void {
+        throw new Error('Method not implemented.');
+    }
 
     onReset() {
         if (!this.email) { 
@@ -24,5 +64,9 @@ export class PasswordResetComponent{
             () => this.msg = "Success", 
             () => this.msg = "Wrong Email"); 
         }
+    }
+
+    onHandleResetPassword(): void {
+        throw new Error('Method not implemented.');
     }
 }

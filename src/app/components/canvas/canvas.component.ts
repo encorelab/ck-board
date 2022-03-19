46d81aa956df7b87cc69ec04830545c1f1e23d72
StevenWuzz <steven.limantoro@gmail.com>
2022-03-19 13:10:40 -0400

    fix: adjusted pinch zoom sensitivity

--- src/app/components/canvas/canvas.component.ts
+++ src/app/components/canvas/canvas.component.ts
@@ -531,7 +531,7 @@ export class CanvasComponent {
           this.zoom *= 0.999 ** delta;
         }
         else {
-          this.zoom *= 0.95 ** delta;
+          this.zoom *= 0.99 ** delta;
         }
         if (this.zoom > 20) this.zoom = 20;
         if (this.zoom < 0.01) this.zoom = 0.01;

28e27cd714bc831bfc251649e350d6d133894f66
Ammar <ammart500@gmail.com>
2022-05-07 16:17:57 -0400

    Remove old firebase calls for likes/comments

--- frontend/src/app/services/comment.service.ts
+++ frontend/src/app/services/comment.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import Comment from '../models/comment';
 
 @Injectable({
@@ -8,26 +7,7 @@ import Comment from '../models/comment';
 })
 export class CommentService {
 
-  private commentsPath : string = 'comments';
-  commentCollection: AngularFirestoreCollection<Comment>;
-
-  constructor(private db: AngularFirestore, private http: HttpClient) {
-    this.commentCollection = db.collection<Comment>(this.commentsPath);
-  }
-
-  observable(boardID: string, handleAdd: Function, skipFirst: boolean) {
-    return this.commentCollection.ref.where("boardID", "==", boardID).onSnapshot((snapshot) => {
-      if (!skipFirst) {
-        snapshot.docChanges().forEach((change) => {
-          if (change.type === "added") {
-            handleAdd(change.doc.data())
-          }
-        })
-      } else {
-        skipFirst = false
-      }
-    })
-  }
+  constructor(private http: HttpClient) {}
 
   getCommentsByPost(postID: string): Promise<Comment[]> {
     return this.http.get<Comment[]>('comments/posts/' + postID).toPromise();
@@ -35,9 +15,5 @@ export class CommentService {
 
   add(comment: Comment): any {
     return this.http.post('comments/', {comment}).toPromise();
-  }
-
-  remove(commentID: string) {
-    return this.commentCollection.ref.doc(commentID).delete()
   }
 } 
--- frontend/src/app/services/likes.service.ts
+++ frontend/src/app/services/likes.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import Like from '../models/like';
 
 @Injectable({
@@ -8,28 +7,7 @@ import Like from '../models/like';
 })
 export class LikesService {
 
-  private likesPath : string = 'likes';
-  likesCollection: AngularFirestoreCollection<Like>;
-
-  constructor(private db: AngularFirestore, private http: HttpClient) {
-    this.likesCollection = db.collection<Like>(this.likesPath);
-  }
-
-  observable(boardID: string, handleChange: Function, skipFirst: boolean) {
-    return this.likesCollection.ref.where("boardID", "==", boardID).onSnapshot((snapshot) => {
-      if (!skipFirst) {
-        snapshot.docChanges().forEach((change) => {
-          if (change.type === "added") {
-            handleChange(change.doc.data(), "added")
-          } else if (change.type == "removed") {
-            handleChange(change.doc.data(), "removed")
-          }
-        })
-      } else {
-        skipFirst = false
-      }
-    })
-  }
+  constructor(private http: HttpClient) {}
 
   getLikesByPost(postID: string): Promise<Like[]> {
     return this.http.get<Like[]>('likes/posts/' + postID).toPromise();

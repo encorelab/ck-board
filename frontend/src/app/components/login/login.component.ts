ff139cfb96bf6bca6458a84aa583f3acc7685497
Abubukker Chaudhary <abubukker.chaudhary@gmail.com>
2023-03-03 21:25:09 -0500

    (FIX): Redirect to initial URL upon login

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -17,7 +17,9 @@ export class LoginComponent {
   invalidCredentials = false;
 
   constructor(private userService: UserService, private router: Router) {
-    if (this.userService.loggedIn) this.router.navigate(['/dashboard']);
+    if (this.userService.loggedIn) {
+      this.router.navigate(['/dashboard']);
+    }
   }
 
   onLogin() {
@@ -28,7 +30,21 @@ export class LoginComponent {
         const redirectUrl = this.userService.redirectUrl;
         this.userService.redirectUrl = null;
         if (redirectUrl) {
-          this.router.navigate([redirectUrl]);
+          // Parse query parameters (if they exist)
+          const searchParams =
+            redirectUrl.indexOf('?') != -1
+              ? new URLSearchParams(
+                  redirectUrl.substring(redirectUrl.indexOf('?'))
+                )
+              : [];
+
+          this.router.navigate([`${redirectUrl.split('?')[0]}`], {
+            queryParams: [...searchParams].reduce((o, [key, value]) => {
+              o[key] = value;
+              return o;
+            }, {}),
+            queryParamsHandling: 'merge',
+          });
         } else {
           this.router.navigate(['dashboard']);
         }

251d9ec0de35ac52b6c4e285d8202061f8fe0f01
Ammar <ammart500@gmail.com>
2023-04-28 11:42:07 -0400

    Add migration doc + fix up signature

--- backend/migrations/20230117192943-set-post-colors.ts
+++ backend/migrations/20230117192943-set-post-colors.ts
@@ -1,83 +1,99 @@
-import { Db } from 'mongodb';
+import { Db, MongoClient } from 'mongodb';
 import { STUDENT_POST_COLOR, TEACHER_POST_COLOR } from '../src/constants';
 import { PostModel } from '../src/models/Post';
 import { Role, UserModel } from '../src/models/User';
 import { BoardModel } from '../src/models/Board';
 
-export const up = async (db: Db) => {
-  const boardCursor = db.collection<BoardModel>('boards').find();
-  while (await boardCursor.hasNext()) {
-    const board = await boardCursor.next();
-    const postsCursor = db
-      .collection<PostModel>('posts')
-      .find({ boardID: board?.boardID });
-    while (await postsCursor.hasNext()) {
-      const post = await postsCursor.next();
-      const userID = post?.userID;
-      const user = await db
-        .collection<UserModel>('users')
-        .findOne({ userID: userID });
-      if (!user || !post) continue;
+export const up = async (db: Db, client: MongoClient) => {
+  const session = client.startSession();
 
-      if (user.role == Role.STUDENT) {
-        if (!post.displayAttributes) {
-          await db
-            .collection<PostModel>('posts')
-            .updateOne(
-              { postID: post.postID },
-              { $set: { displayAttributes: { fillColor: STUDENT_POST_COLOR } } }
-            );
-        } else {
-          await db
-            .collection<PostModel>('posts')
-            .updateOne(
-              { postID: post.postID },
-              { $set: { 'displayAttributes.fillColor': STUDENT_POST_COLOR } }
-            );
-        }
-      } else {
-        if (!post.displayAttributes) {
-          await db
-            .collection<PostModel>('posts')
-            .updateOne(
-              { postID: post.postID },
-              { $set: { displayAttributes: { fillColor: TEACHER_POST_COLOR } } }
-            );
-        } else {
-          await db
-            .collection<PostModel>('posts')
-            .updateOne(
-              { postID: post.postID },
-              { $set: { 'displayAttributes.fillColor': TEACHER_POST_COLOR } }
-            );
+  try {
+    await session.withTransaction(async () => { 
+      const boardCursor = db.collection<BoardModel>('boards').find();
+      while (await boardCursor.hasNext()) {
+        const board = await boardCursor.next();
+        const postsCursor = db
+          .collection<PostModel>('posts')
+          .find({ boardID: board?.boardID });
+        while (await postsCursor.hasNext()) {
+          const post = await postsCursor.next();
+          const userID = post?.userID;
+          const user = await db
+            .collection<UserModel>('users')
+            .findOne({ userID: userID });
+          if (!user || !post) continue;
+    
+          if (user.role == Role.STUDENT) {
+            if (!post.displayAttributes) {
+              await db
+                .collection<PostModel>('posts')
+                .updateOne(
+                  { postID: post.postID },
+                  { $set: { displayAttributes: { fillColor: STUDENT_POST_COLOR } } }
+                );
+            } else {
+              await db
+                .collection<PostModel>('posts')
+                .updateOne(
+                  { postID: post.postID },
+                  { $set: { 'displayAttributes.fillColor': STUDENT_POST_COLOR } }
+                );
+            }
+          } else {
+            if (!post.displayAttributes) {
+              await db
+                .collection<PostModel>('posts')
+                .updateOne(
+                  { postID: post.postID },
+                  { $set: { displayAttributes: { fillColor: TEACHER_POST_COLOR } } }
+                );
+            } else {
+              await db
+                .collection<PostModel>('posts')
+                .updateOne(
+                  { postID: post.postID },
+                  { $set: { 'displayAttributes.fillColor': TEACHER_POST_COLOR } }
+                );
+            }
+          }
         }
       }
-    }
+    });
+  } finally {
+    session.endSession();
   }
 };
 
-export const down = async (db: Db) => {
-  const boardCursor = db.collection<BoardModel>('boards').find();
-  while (await boardCursor.hasNext()) {
-    const board = await boardCursor.next();
-    const postsCursor = db
-      .collection<PostModel>('posts')
-      .find({ boardID: board?.boardID });
-    while (await postsCursor.hasNext()) {
-      const post = await postsCursor.next();
-      const userID = post?.userID;
-      const user = await db
-        .collection<UserModel>('users')
-        .findOne({ userID: userID });
-      if (!user || !post) continue;
-      if (post.displayAttributes) {
-        await db
+export const down = async (db: Db, client: MongoClient) => {
+  const session = client.startSession();
+
+  try {
+    await session.withTransaction(async () => {
+      const boardCursor = db.collection<BoardModel>('boards').find();
+      while (await boardCursor.hasNext()) {
+        const board = await boardCursor.next();
+        const postsCursor = db
           .collection<PostModel>('posts')
-          .updateOne(
-            { postID: post.postID },
-            { $set: { 'displayAttributes.fillColor': STUDENT_POST_COLOR } }
-          );
+          .find({ boardID: board?.boardID });
+        while (await postsCursor.hasNext()) {
+          const post = await postsCursor.next();
+          const userID = post?.userID;
+          const user = await db
+            .collection<UserModel>('users')
+            .findOne({ userID: userID });
+          if (!user || !post) continue;
+          if (post.displayAttributes) {
+            await db
+              .collection<PostModel>('posts')
+              .updateOne(
+                { postID: post.postID },
+                { $set: { 'displayAttributes.fillColor': STUDENT_POST_COLOR } }
+              );
+          }
+        }
       }
-    }
+    });
+  } finally {
+    session.endSession();
   }
 };

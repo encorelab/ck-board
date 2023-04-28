28b0fa3073ed1de16dfbda6d93924c31071487d7
Ammar <ammart500@gmail.com>
2023-04-28 11:44:28 -0400

    Styling

--- backend/migrations/20230117192943-set-post-colors.ts
+++ backend/migrations/20230117192943-set-post-colors.ts
@@ -8,7 +8,7 @@ export const up = async (db: Db, client: MongoClient) => {
   const session = client.startSession();
 
   try {
-    await session.withTransaction(async () => { 
+    await session.withTransaction(async () => {
       const boardCursor = db.collection<BoardModel>('boards').find();
       while (await boardCursor.hasNext()) {
         const board = await boardCursor.next();
@@ -22,21 +22,27 @@ export const up = async (db: Db, client: MongoClient) => {
             .collection<UserModel>('users')
             .findOne({ userID: userID });
           if (!user || !post) continue;
-    
+
           if (user.role == Role.STUDENT) {
             if (!post.displayAttributes) {
               await db
                 .collection<PostModel>('posts')
                 .updateOne(
                   { postID: post.postID },
-                  { $set: { displayAttributes: { fillColor: STUDENT_POST_COLOR } } }
+                  {
+                    $set: {
+                      displayAttributes: { fillColor: STUDENT_POST_COLOR },
+                    },
+                  }
                 );
             } else {
               await db
                 .collection<PostModel>('posts')
                 .updateOne(
                   { postID: post.postID },
-                  { $set: { 'displayAttributes.fillColor': STUDENT_POST_COLOR } }
+                  {
+                    $set: { 'displayAttributes.fillColor': STUDENT_POST_COLOR },
+                  }
                 );
             }
           } else {
@@ -45,14 +51,20 @@ export const up = async (db: Db, client: MongoClient) => {
                 .collection<PostModel>('posts')
                 .updateOne(
                   { postID: post.postID },
-                  { $set: { displayAttributes: { fillColor: TEACHER_POST_COLOR } } }
+                  {
+                    $set: {
+                      displayAttributes: { fillColor: TEACHER_POST_COLOR },
+                    },
+                  }
                 );
             } else {
               await db
                 .collection<PostModel>('posts')
                 .updateOne(
                   { postID: post.postID },
-                  { $set: { 'displayAttributes.fillColor': TEACHER_POST_COLOR } }
+                  {
+                    $set: { 'displayAttributes.fillColor': TEACHER_POST_COLOR },
+                  }
                 );
             }
           }

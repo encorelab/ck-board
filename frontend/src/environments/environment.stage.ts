2a12781ae4a6ad902ca69cebf3709043c37ccaad
JoelWiebe <joel.p.wiebe@gmail.com>
2024-10-22 20:17:27 -0400

    Removed extra / from ckboardDomain in staging env vars

--- frontend/src/environments/environment.stage.ts
+++ frontend/src/environments/environment.stage.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false, //  Important: Set this to false for staging
     apiUrl: 'https://ck-board-188ef16f21a5.herokuapp.com/api', 
     socketUrl: 'https://ck-board-188ef16f21a5.herokuapp.com:8000', 
-    ckboardDomain: 'https://ck-board-188ef16f21a5.herokuapp.com/' 
+    ckboardDomain: 'https://ck-board-188ef16f21a5.herokuapp.com' 
   };

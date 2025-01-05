9d23c385197140b0ad4c2accab57784c096c5a28
markiianbabiak <markiian.babiak@torontomu.ca>
2025-01-04 19:22:08 -0500

    Tracing Bug Fix
    When creating a post and adding a tag in one action, the tag addition wouldn't be logged - Fixed

--- backend/src/socket/trace/post.trace.ts
+++ backend/src/socket/trace/post.trace.ts
@@ -18,12 +18,29 @@ import { createTrace } from './base.trace';
 const create = async (input: SocketPayload<PostModel>, eventType: string) => {
   const trace = await createTrace(input.trace);
   const post = input.eventData;
-  trace.event = {
-    postID: post.postID,
-    postModifiedTitle: post.title,
-    postModifiedMessage: post.desc,
-  };
+  const tagNames: string[] = [];
+  const tagIDs: string[] = [];
+  if (post.tags.length > 0) {
+    post.tags.forEach((tag) => {
+      tagNames.push(tag.name);
+      tagIDs.push(tag.tagID);
+    });
+    trace.event = {
+      postID: post.postID,
+      postModifiedTitle: post.title,
+      postModifiedMessage: post.desc,
+      postTagNameAdded: tagNames.toString(),
+      postTagIDAdded: tagIDs.toString(),
+    };
+  } else {
+    trace.event = {
+      postID: post.postID,
+      postModifiedTitle: post.title,
+      postModifiedMessage: post.desc,
+    };
+  }
   trace.eventType = eventType;
+
   return dalTrace.create(trace);
 };
 

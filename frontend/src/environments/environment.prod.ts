469c6731678474363fae6020dc00caa49992f70e
markiianbabiak <markiian.babiak@torontomu.ca>
2025-02-01 12:12:10 -0500

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ck-board.oise.utoronto.ca/api',
-  socketUrl: 'https://ck-board.oise.utoronto.ca',
-  ckboardDomain: 'https://ck-board.oise.utoronto.ca',
+  apiUrl: 'https://ck-board-app-ky2q7.ondigitalocean.app/api',
+  socketUrl: 'https://ck-board-app-ky2q7.ondigitalocean.app',
+  ckboardDomain: 'https://ck-board-app-ky2q7.ondigitalocean.app',
 };

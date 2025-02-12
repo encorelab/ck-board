4627f4ab47da970237d8bc6ba03c0e1b34097c87
markiianbabiak <markiian.babiak@torontomu.ca>
2025-02-11 20:07:50 -0500

    Invalid password length feature

--- backend/src/api/auth.ts
+++ backend/src/api/auth.ts
@@ -84,7 +84,7 @@ router.post('/forgot-password', async (req, res) => {
 
     if (!user) {
       // Don't reveal if the user exists or not for security reasons
-      return res.status(404).send({ message: 'No user found with that email' }); 
+      return res.status(404).send({ message: 'No user found with that email' });
     }
 
     // 3. Generate a unique, one-time use token (e.g., using UUID)
@@ -92,24 +92,42 @@ router.post('/forgot-password', async (req, res) => {
 
     // 4. Store the token with an expiration time (e.g., 1 hour) in the database, associated with the user
     try {
-      await dalUser.update(user.userID, { resetPasswordToken: resetToken, resetPasswordExpires: addHours(1) });
+      await dalUser.update(user.userID, {
+        resetPasswordToken: resetToken,
+        resetPasswordExpires: addHours(1),
+      });
     } catch (error) {
-      console.error("Error updating user in database:", error);
-      return res.status(500).send({ message: 'An internal server error occurred.' });
+      console.error('Error updating user in database:', error);
+      return res
+        .status(500)
+        .send({ message: 'An internal server error occurred.' });
     }
 
     // 5. Send an email to the user with a link containing the token
     const resetLink = `${process.env.CKBOARD_SERVER_ADDRESS}/reset-password?token=${resetToken}`;
 
     try {
-        await generateEmail(email, 'Password Reset Request', resetLink)
-        return res.status(200).send({ success: true, message: 'If an account with that email exists, a password reset link has been sent.' });
-    } catch(err) {
-        return res.status(500).send({ success: false, message: 'There was an error sending the password reset email.' })
+      await generateEmail(email, 'Password Reset Request', resetLink);
+      return res
+        .status(200)
+        .send({
+          success: true,
+          message:
+            'If an account with that email exists, a password reset link has been sent.',
+        });
+    } catch (err) {
+      return res
+        .status(500)
+        .send({
+          success: false,
+          message: 'There was an error sending the password reset email.',
+        });
     }
   } catch (error) {
-    console.error("Error in /forgot-password route:", error);
-    return res.status(500).send({ message: 'An internal server error occurred.' }); 
+    console.error('Error in /forgot-password route:', error);
+    return res
+      .status(500)
+      .send({ message: 'An internal server error occurred.' });
   }
 });
 
@@ -119,8 +137,14 @@ router.post('/reset-password', async (req, res) => {
   // 1. Find the user by the reset token
   const user = await dalUser.findByPasswordResetToken(token);
 
-  if (!user || !user.resetPasswordExpires || user.resetPasswordExpires < new Date()) {
-    return res.status(400).send({ message: 'Invalid or expired password reset token' });
+  if (
+    !user ||
+    !user.resetPasswordExpires ||
+    user.resetPasswordExpires < new Date()
+  ) {
+    return res
+      .status(400)
+      .send({ message: 'Invalid or expired password reset token' });
   }
 
   // 2. Hash the new password
--- frontend/src/app/components/reset-password/reset-password.component.ts
+++ frontend/src/app/components/reset-password/reset-password.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { UntypedFormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
+import { MyErrorStateMatcher } from 'src/app/utils/ErrorStateMatcher';
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.scss']
+  styleUrls: ['./reset-password.component.scss'],
 })
 export class ResetPasswordComponent implements OnInit {
   token: string = '';
@@ -13,14 +15,22 @@ export class ResetPasswordComponent implements OnInit {
   confirmPassword: string = '';
   message: string = '';
 
+  passwordControl = new UntypedFormControl('', [
+    Validators.required,
+    Validators.minLength(12),
+    Validators.maxLength(30),
+  ]);
+  matcher = new MyErrorStateMatcher();
+  invalidCredentials = false;
+
   constructor(
     private route: ActivatedRoute,
     private userService: UserService,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       this.token = params['token'];
       if (!this.token) {
         // Handle invalid or missing token (e.g., redirect to forgot password page)
@@ -30,19 +40,28 @@ export class ResetPasswordComponent implements OnInit {
   }
 
   onSubmit() {
-    if (this.newPassword !== this.confirmPassword) {
+    if (this.newPassword.length < 12 || this.newPassword.length > 30) {
+      this.invalidCredentials = true;
+      this.message =
+        'Unable to change the password. Password must be between 12 and 30 characters.';
+      return;
+    }
+
+    if (this.newPassword != this.confirmPassword) {
+      this.invalidCredentials = true;
       this.message = 'Passwords do not match.';
       return;
     }
 
-    this.userService.resetPassword(this.token, this.newPassword)
+    this.userService
+      .resetPassword(this.token, this.newPassword)
       .then(() => {
         this.message = 'Password reset successfully. Redirecting to login...';
         setTimeout(() => {
           this.router.navigate(['/login']);
         }, 3000);
       })
-      .catch(error => {
+      .catch((error) => {
         this.message = 'An error occurred. Please try again later.';
         console.error(error);
       });

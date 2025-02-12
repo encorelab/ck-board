2604cf20cd7059a61e2ab232d7c6d3867a340816
markiianbabiak <markiian.babiak@torontomu.ca>
2025-02-11 20:51:21 -0500

    Fixing styling

--- frontend/src/app/components/forgot-password/forgot-password.component.ts
+++ frontend/src/app/components/forgot-password/forgot-password.component.ts
@@ -4,20 +4,25 @@ import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+  styleUrls: ['./forgot-password.component.scss'],
 })
 export class ForgotPasswordComponent {
   email: string = '';
   message: string = '';
+  invalidCredentials = false;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService) {}
 
   onSubmit() {
-    this.userService.requestPasswordReset(this.email)
+    this.userService
+      .requestPasswordReset(this.email)
       .then(() => {
-        this.message = 'If an account with that email exists, a password reset link has been sent.';
+        this.invalidCredentials = false;
+        this.message =
+          'If an account with that email exists, a password reset link has been sent.';
       })
-      .catch(error => {
+      .catch((error) => {
+        this.invalidCredentials = true;
         this.message = 'An error occurred. Please try again later.';
         console.error(error);
       });
